64f0e8e1c07b1b75158960205953d38e2c8bfca4
bruce mckenzie <bjmckenz@gmail.com>
2024-04-14 19:10:51 -0700

    incomplete, wrong auth (going to switch to cookies)

--- src/lib/authStore.js
+++ src/lib/authStore.js
@@ -1,5 +1,41 @@
+import cookie from 'cookie';
+import { browser } from '$app/environment';
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import { writable } from 'svelte/store';
+import { auth } from '../firebase/client';
 
-const authUser = writable(undefined);
+export const user = writable(null);
 
-export { authUser };
+export async function signOut() {
+	return auth.signOut();
+}
+
+export async function signIn() {
+	await signInWithRedirect(auth, new GoogleAuthProvider());
+}
+
+if (browser) {
+	auth.onIdTokenChanged(async (newUser) => {
+		const tokenCurrentlySet = cookie.parse(document.cookie)['token'] !== undefined;
+		const token = newUser ? await newUser?.getIdToken() : undefined;
+		document.cookie = cookie.serialize('token', token ?? '', {
+			path: '/',
+			maxAge: token ? undefined : 0
+		});
+		user.set(newUser);
+
+		if (!tokenCurrentlySet && token) {
+			document.location.reload();
+		}
+	});
+
+	// refresh the ID token every 10 minutes
+	setInterval(
+		async () => {
+			if (auth.currentUser) {
+				await auth.currentUser.getIdToken(true);
+			}
+		},
+		10 * 60 * 1000
+	);
+}
